ae424e18fc00c26a423f70efa30279c56f6fddfc
01010111 <Will@Wills-MBP.fios-router.home>
2017-12-30 16:32:10 -0500

    Added ZOgmoLoader

---
+++ zero/flxutil/editors/ZOgmoLoader.hx
@@ -0,0 +1,15 @@
+package zero.flxutil.editors;
+
+import flixel.addons.editors.ogmo.FlxOgmoLoader;
+
+using zero.ext.StringExt;
+
+class ZOgmoLoader extends FlxOgmoLoader
+{
+
+    public function get_tilemap_array(tile_layer:String = 'tiles'):Array<Array<Int>>
+    {
+        return _fastXml.node.resolve(tile_layer).innerData.csv_to_2d_int_array();
+    }
+
+}
