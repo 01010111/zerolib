46d8b91ad9f3783aa53ad8f53be72fd60ecc2d80
Will Blanton <will.blanton.804@gmail.com>
2018-01-20 20:11:35 -0500

    added compare() to IntPoint

--- zero/util/IntPoint.hx
+++ zero/util/IntPoint.hx
@@ -12,6 +12,11 @@ class IntPoint
         this.y = y == null ? x : y;
     }
 
+    public function compare(ip:IntPoint):Bool
+    {
+        return (ip.x == x && ip.y == y);
+    }
+
     public static function add(ip1, ip2):IntPoint
     {
         return new IntPoint(ip1.x + ip2.x, ip1.y + ip2.y);
