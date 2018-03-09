c4c4cff11202ee8a2125e0be4f29d17a470202cd
01010111 <will.blanton.804@gmail.com>
2018-03-09 13:05:12 -0500

    StringExt get_random()

--- zero/ext/StringExt.hx
+++ zero/ext/StringExt.hx
@@ -1,6 +1,7 @@
 package zero.ext;
 
 using zero.ext.ArrayExt;
+using zero.ext.FloatExt;
 
 /**
  *  @author 01010111 
@@ -8,6 +9,11 @@ using zero.ext.ArrayExt;
 class StringExt
 {
 
+    /**
+     *  returns a 2D array of integers from a csv string
+     *  @param csv - 
+     *  @return Array<Array<Int>>
+     */
     inline public static function csv_to_2d_int_array(csv:String):Array<Array<Int>>
     {
         var a:Array<Array<Int>> = [];
@@ -15,5 +21,13 @@ class StringExt
         for (row in rows) a.push(row.split(',').strings_to_ints());
         return a;
     }
+
+	inline public static function get_random(string:String, length:Int = 16, prefix:String = '', postfix:String = '')
+	{
+		var a = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~`!@#$%^&*()_+-={}|[]:";<>?,./';
+		var s = '';
+		for (i in 0...length)s += a.charAt(a.length.get_random());
+		return '$prefix$s$postfix';
+	}
 
 }
