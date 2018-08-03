8a11f1cbea6ef6d2f2e421bdfebfb06e1e4e2169
01010111 <will.blanton.804@gmail.com>
2018-08-03 16:18:18 -0400

    Added TopDownWalker component

---
+++ zero/flxutil/ecs/components/TopDownWalker.hx
@@ -0,0 +1,57 @@
+package zero.flxutil.ecs.components;
+
+import zero.flxutil.input.Controller;
+import zero.flxutil.ecs.Component;
+
+using Math;
+
+/**
+ * A Component to make an entity in a top down game Walk
+ */
+class TopDownWalker extends Component
+{
+
+	var controller:Controller;
+	var walk_speed:Float;
+	var accel_amt:Float;
+	var drag_amt:Float;
+
+	/**
+	 * Create a new Walker component with options
+	 * @param options 
+	 */
+	public function new(options:TopDownWalkerOptions)
+	{
+		super('top_down_walker');
+
+		controller = options.controller;
+		walk_speed = options.walk_speed;
+		accel_amt = options.acceleration_force;
+		drag_amt = options.drag_force;
+	}
+
+	override function on_add()
+	{
+		entity.maxVelocity.set(walk_speed, walk_speed);
+		drag.set(drag_amt, drag_amt);
+	}
+
+	override public function update(dt:Float)
+	{
+		acceleration.set();
+		if (controller.pressed(DPAD_UP)) acceleration.x -= accel_amt;
+		if (controller.pressed(DPAD_DOWN)) acceleration.x += accel_amt;
+		if (controller.pressed(DPAD_LEFT)) acceleration.x -= accel_amt;
+		if (controller.pressed(DPAD_RIGHT)) acceleration.x += accel_amt;
+		if ((velocity.x.abs() - acceleration.x.abs()).abs() > 1) velocity.x *= 0.5;
+		if ((velocity.y.abs() - acceleration.y.abs()).abs() > 1) velocity.y *= 0.5;
+	}
+}
+
+typedef TopDownWalkerOptions = 
+{
+	controller:Controller,
+	walk_speed:Float,
+	acceleration_force:Float,
+	drag_force:Float
+}
