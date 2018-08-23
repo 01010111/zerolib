9d8886b554d1675ae6e053a8531a47650c917c81
01010111 <will.blanton.804@gmail.com>
2018-08-23 10:26:09 -0400

    ZFONTS

--- zero/flxutil/ui/BitmapText.hx
+++ zero/flxutil/ui/BitmapText.hx
@@ -1,5 +1,7 @@
 package zero.flxutil.ui;
 
+import haxe.Json;
+import openfl.Assets;
 import flixel.graphics.frames.FlxBitmapFont;
 import flixel.math.FlxPoint;
 import flixel.system.FlxAssets;
@@ -10,6 +12,7 @@ import openfl.display.BitmapData;
 import haxe.Utf8;
 
 using Math;
+using StringTools;
 
 /**
  * A wrapper for FlxBitmapText that makes it easy to use a monospaced bitmap font image
@@ -22,8 +25,6 @@ class BitmapText extends FlxBitmapText
 	 */
 	public function new(options:BitmapTextOptions) 
 	{
-		trace(Utf8.validate(options.charset));
-		for (char in options.charset.split('')) trace(char, Utf8.validate(char));
 		var font:FlxBitmapFont = FlxBitmapFont.fromMonospace(options.graphic, options.charset, options.letter_size);
 		super(font);
 		autoSize = false;
@@ -95,6 +96,62 @@ class BitmapText extends FlxBitmapText
 		return data;
 	}
 
+	/**
+	 * Creates a new bitmaptext instance from the ZFont schematic
+	 * @param options 
+	 * @return Null<BitmapText>
+	 */
+	public static function from_zfont(options:ZFontOptions):Null<BitmapText>
+	{
+		if (options.zfont.trim().charAt(0) != '{') options.zfont = Assets.getText(options.zfont);
+		var zfont:ZFont = Json.parse(options.zfont);
+		if (!validate_zfont(zfont)) return null;
+		if (options.colors == null) options.colors = { zero: 0x00FFFFFF, one: 0xFFFFFFFF };
+		return new BitmapText({
+			charset: [for (char in zfont.chars) char.char].join(''),
+			letter_size: FlxPoint.get(zfont.size.x, zfont.size.y),
+			graphic: create_graphic_from_zfont(zfont, options.colors),
+			position: options.position,
+			align: options.align,
+			width: options.width,
+			letter_spacing: options.letter_spacing,
+			line_spacing: options.line_spacing,
+			scroll_factor: options.scroll_factor
+		});
+	}
+
+	static function validate_zfont(font:ZFont):Bool
+	{
+		for (char in font.chars) if (char.data.length != font.size.x * font.size.y) return false;
+		return true;
+	}
+
+	static function create_graphic_from_zfont(font:ZFont, colors:ZFontColors):FlxBitmapFontGraphicAsset
+	{
+		var size = font.chars.length.sqrt().ceil();
+		var data = new BitmapData(size * font.size.x, size * font.size.y, true, 0x00FFFFFF);
+		var x = 0;
+		var y = 0;
+		for (char in font.chars)
+		{
+			for (j in 0...font.size.y) for (i in 0...font.size.x)
+				(char.data.charAt(i + j * font.size.x) == '1') 
+				? data.setPixel32(x * font.size.x + i, y * font.size.y + j, colors.one)
+				: data.setPixel32(x * font.size.x + i, y * font.size.y + j, colors.zero);
+			x++;
+			if (x < size) continue;
+			x = 0;
+			y++;
+		}
+		return data;
+	}
+
+}
+
+class ZFonts
+{
+	public static var FONT_SQUARE:String = "FONT_SQUARE";
+	public static var FONT_NES:String = "FONT_NES";
 }
 
 typedef BitmapTextOptions =
@@ -125,18 +182,49 @@ typedef BitsyFont =
 {
 	font:String,
 	size:BitsySize,
-	chars:Array<CharData>
+	chars:Array<BitsyCharData>,
 }
 
 typedef BitsySize =
 {
 	x:Int,
-	y:Int
+	y:Int,
 }
 
-typedef CharData = 
+typedef BitsyCharData = 
 {
 	id:Int,
 	char:String,
-	data:Array<Array<Int>>
+	data:Array<Array<Int>>,
+}
+
+typedef ZFontOptions =
+{
+	zfont:String,
+	?colors:ZFontColors,
+	?position:FlxPoint,
+	?align:FlxTextAlign,
+	?width:Int,
+	?letter_spacing:Int,
+	?line_spacing:Int,
+	?scroll_factor:FlxPoint,
+}
+
+typedef ZFontColors =
+{
+	one:Int,
+	zero:Int,
+}
+
+typedef ZFont =
+{
+	name:String,
+	size:BitsySize,
+	chars:Array<ZCharData>,
+}
+
+typedef ZCharData =
+{
+	char:String,
+	data:String,
 }
