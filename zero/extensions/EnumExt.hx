f01aed84cadd0aa09389fe3f92a223ce63d5a142
Will Blanton <will@Wills-MacBook-Pro.local>
2019-09-16 11:11:27 -0400

    :book:

--- zero/extensions/EnumExt.hx
+++ zero/extensions/EnumExt.hx
@@ -3,6 +3,11 @@ package zero.extensions;
 class EnumExt
 {
 
+	/**
+	 * Get an array of all enums, shortcut for Type.allEnums()
+	 * @param e input
+	 * @return Array<T> return [for (e in Type.allEnums(e)) e]
+	 */
 	public static inline function all<T>(e:Enum<T>):Array<T> return [for (e in Type.allEnums(e)) e];
 
 }
