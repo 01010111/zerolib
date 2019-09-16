11df9e9462e5bf12fa557befe629b577c0ad795d
Will Blanton <will@Wills-MacBook-Pro.local>
2019-09-16 11:10:14 -0400

    Added EventBus

--- zero/utilities/EventBus.hx
+++ zero/utilities/EventBus.hx
@@ -1,23 +1,25 @@
 package zero.utilities;
 
+using Std;
+
 class EventBus
 {
 
-	static var listeners:Map<String, Array<Dynamic -> Void>> = new Map();
+	static var listeners:Map<String, Array<?Dynamic -> Void>> = new Map();
 
-	public static function dispatch(name:String, data:Dynamic) {
-		if (!listeners.exists(name)) return;
-		for (listener in listeners[name]) if (listener != null) listener(data);
+	public static function dispatch(name:Dynamic, ?data:Dynamic) {
+		if (!listeners.exists(name.string())) return;
+		for (listener in listeners[name.string()]) if (listener != null) listener(data);
 	}
 
-	public static function register_listener(listener:Dynamic -> Void, name:String) {
-		if (!listeners.exists(name)) listeners.set(name, []);
-		listeners[name].push(listener);
+	public static function register_listener(listener:?Dynamic -> Void, name:Dynamic) {
+		if (!listeners.exists(name.string())) listeners.set(name.string(), []);
+		listeners[name.string()].push(listener);
 	}
 
-	public static function deregister_listener(listener:Dynamic -> Void, name:String) {
-		if (!listeners.exists(name)) return;
-		listeners[name].remove(listener);
+	public static function deregister_listener(listener:?Dynamic -> Void, name:Dynamic) {
+		if (!listeners.exists(name.string())) return;
+		listeners[name.string()].remove(listener);
 	}
 
 	public static function deregister_all() {
