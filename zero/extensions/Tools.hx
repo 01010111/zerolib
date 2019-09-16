86668313155b6aae8d25c4d1d41b16bbc4392265
Will Blanton <will@Wills-MacBook-Pro.local>
2019-09-16 11:58:18 -0400

    added Tools compilation

---
+++ zero/extensions/Tools.hx
@@ -0,0 +1,15 @@
+package zero.extensions;
+
+/**
+ * To use ALL of the zero.extensions, simply:
+ * using zero.extensions.Tools
+ */
+
+@:dox(hide)
+typedef TArrayExt = ArrayExt;
+@:dox(hide)
+typedef TEnumExt = EnumExt;
+@:dox(hide)
+typedef TFloatExt = FloatExt;
+@:dox(hide)
+typedef TStringExt = StringExt;
