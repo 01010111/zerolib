c67b6cc9ae3dc61b67dfdcb08aa7f34a2def57d8
Will Blanton <will@Wills-MacBook-Pro.local>
2020-03-03 15:35:05 -0500

    added set_active()

--- zero/utilities/EventBus.hx
+++ zero/utilities/EventBus.hx
@@ -16,15 +16,17 @@ using Std;
 class EventBus
 {
 
-	static var listeners:Map<String, Array<?Dynamic -> Void>> = new Map();
+	static var listeners:Map<String, Array<?Dynamic -> Void>> = [];
+	static var active_map:Map<String, Bool> = [];
 
 	public static function dispatch(name:String, ?data:Dynamic) {
 		if (!listeners.exists(name)) return;
-		for (listener in listeners[name]) if (listener != null) listener(data);
+		for (listener in listeners[name]) if (listener != null && active_map[name]) listener(data);
 	}
 
 	public static function listen(listener:?Dynamic -> Void, name:String) {
 		if (!listeners.exists(name)) listeners.set(name, []);
+		if (!active_map.exists(name)) active_map.set(name, true);
 		listeners[name].push(listener);
 	}
 
@@ -39,6 +41,11 @@ class EventBus
 
 	public static function unlisten_signal(name:String) {
 		listeners.remove(name);
+	}
+
+	public static function set_active(name:String, active:Bool) {
+		if (!active_map.exists(name)) return;
+		active_map.set(name, active);
 	}
 
 }
